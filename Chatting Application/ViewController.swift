0b2ad2cae910254492f5bc7e3e995489580338c9
Sagar Ali <sagar18.iosdeveloper@gmail.com>
2020-09-05 18:31:09 +0500

    Initial Commit

---
+++ Chatting Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatting Application
+//
+//  Created by Sagar Baloch on 05/09/2020.
+//  Copyright © 2020 Sagar Baloch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
